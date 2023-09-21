5d7dd540b71b3b27398a71be038f476b6bd576dd
Eli Boninger <eboninger@gmail.com>
2023-09-21 14:33:39 -0400

    movement system working with tiles

--- Assets/Scripts/CharlesController.cs
+++ Assets/Scripts/CharlesController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 public class CharlesController : MonoBehaviour
 {
     public float speed = 1;
     public Animator animator;
+    public bool stopMovement = false;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -20,28 +22,45 @@ public class CharlesController : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if (!stopMovement)
         {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
 
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
+            Vector2 move = new Vector2(horizontal, vertical);
+
+            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+            {
+                lookDirection.Set(move.x, move.y);
+                lookDirection.Normalize();
+            }
+
+            animator.SetFloat("Look X", lookDirection.x);
+            animator.SetFloat("Look Y", lookDirection.y);
+            animator.SetFloat("Speed", move.magnitude);
+        }
     }
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        if (!stopMovement)
+        {
+            Vector2 position = rigidbody2d.position;
+            position.x = position.x + speed * horizontal * Time.deltaTime;
+            position.y = position.y + speed * vertical * Time.deltaTime;
 
-        rigidbody2d.MovePosition(position);
+            rigidbody2d.MovePosition(position);
+        }
+
+    }
+
+    public void StopMovement()
+    {
+        stopMovement = true;
+    }
+
+    public void StartMovement()
+    {
+        stopMovement = false;
     }
 }
